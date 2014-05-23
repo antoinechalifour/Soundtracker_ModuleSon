2f558a99b76663f9b6e7f3955d4837b1b4902c6e
antoinechalifour <antoine.chalifour@gmail.com>
2014-05-23 18:17:02 +0200

    Delete StNullException.cpp

--- StNullException.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "StNullException.h"
-#include "StException.h"
-
-StNullException::StNullException() :
-StException(){}
-
-StNullException::StNullException(std::string msg) :
-StException(msg){}
