0e6eabc1f30897e31f9601773a0427d5d878f2d4
antoinechalifour <antoine.chalifour@gmail.com>
2014-05-23 18:17:14 +0200

    Delete StValueException.cpp

--- StValueException.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "StValueException.h"
-#include "StException.h"
-
-StValueException::StValueException() :
-StException(){}
-
-StValueException::StValueException(std::string msg) :
-StException(msg){}
